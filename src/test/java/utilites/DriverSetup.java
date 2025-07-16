39c439b84618095d1e6c10d0709c31258e514c34
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2025-07-16 22:00:51 +0600

    driver setup

--- src/test/java/utilites/DriverSetup.java
+++ src/test/java/utilites/DriverSetup.java
@@ -8,6 +8,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import java.time.Duration;
 
 public class DriverSetup {
+    private static String browser_name = System.getProperty("browser", "chrome");
+
+    private static final ThreadLocal<WebDriver> DRIVER_THREAD_LOCAL = new ThreadLocal<>();
 }
