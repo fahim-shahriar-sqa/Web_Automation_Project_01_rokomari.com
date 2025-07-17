abec61dd69376a4c569c2131fafeae131beefc19
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2025-07-17 19:16:07 +0600

    Base page setup

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,4 +1,42 @@
 package pages;
 
+import static utilites.DriverSetup.getDriver;
+
 public class BasePage {
+
+    public void loadAPage(String url) {
+        getDriver().get(url);
+    }
+
+    public String getPageUrl() {
+        return getDriver().getCurrentUrl();
+    }
+
+    public String getPageTitle() {
+        return getDriver().getTitle();
+    }
+
+    public void setWindowToFullscreen() {
+        getDriver().manage().window().fullscreen();
+    }
+
+    public void setWindowToMinimize() {
+        getDriver().manage().window().minimize();
+    }
+
+    public void setWindowToMaximize() {
+        getDriver().manage().window().maximize();
+    }
+
+    public void refresh(){
+        getDriver().navigate().refresh();
+    }
+
+    public void goBack(){
+        getDriver().navigate().back();
+    }
+
+    public void goForward(){
+        getDriver().navigate().forward();
+    }
 }
--- src/test/java/utilites/DriverSetup.java
+++ src/test/java/utilites/DriverSetup.java
@@ -5,9 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import java.time.Duration;
 
 public class DriverSetup {
