99993cab4a1ad9c5789e0f1a56bd815323a38380
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2025-07-17 19:16:56 +0600

    Base page setup

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,5 +1,13 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 import static utilites.DriverSetup.getDriver;
 
 public class BasePage {
@@ -38,5 +46,42 @@ public class BasePage {
 
     public void goForward(){
         getDriver().navigate().forward();
+    }
+
+    public void switchToNewTab(){
+        getDriver().switchTo().newWindow(WindowType.TAB);
+    }
+
+    public void switchToNewWindow(){
+        getDriver().switchTo().newWindow(WindowType.WINDOW);
+    }
+
+    public void windowHandle(){
+        getDriver().getWindowHandle();
+    }
+
+    public WebElement getElement(By locator) {
+        return getDriver().findElement(locator);
+    }
+
+    public void writeOnElement(By locator, String text){
+        getElement(locator).sendKeys(text);
+    }
+
+    public boolean visibleState(By locator){
+        return getElement(locator).isDisplayed();
+    }
+
+    public String getElementText(By locator){
+        return getElement(locator).getText();
+    }
+
+    public void clickOnElement(By locator){
+        getElement(locator).click();
+    }
+
+    public WebElement waitForElementVisible(By locator){
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 }
